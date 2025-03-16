cf594fe920d1a54da22b3f7af3af4f50e19181fd
Gernot Steinegger <Gernot.Steinegger@roomle.com>
2025-03-16 22:40:41 +0100

    refactor: path tracer default light sources

--- src/scene/path-trace-default-light-factory.ts
+++ src/scene/path-trace-default-light-factory.ts
@@ -4,24 +4,19 @@ import { HemisphereLight, Light, RectAreaLight } from 'three/webgpu';
 export class PathTraceDefaultLightFactory implements LightFactory {
   public create(): Promise<Light[]> {
     const environmentLight = new HemisphereLight(0x404040, 0xffffff, 2);
-    const lightSourceTop = new RectAreaLight(0xffffff, 100, 100, 100);
+    const lightSourceTop = new RectAreaLight(0xffffff, 80, 100, 100);
     lightSourceTop.position.set(0, 500, 0);
     lightSourceTop.lookAt(0, 0, 0);
-    const lightSource2 = new RectAreaLight(0xffffff, 60, 100, 100);
-    lightSource2.position.set(-400, 400, 200);
-    lightSource2.lookAt(0, 0, 0);
-    const lightSource3 = new RectAreaLight(0xffffff, 60, 100, 100);
-    lightSource3.position.set(400, 400, 200);
-    lightSource3.lookAt(0, 0, 0);
-    const lightSource4 = new RectAreaLight(0xffffff, 60, 100, 100);
-    lightSource4.position.set(0, 400, -500);
-    lightSource4.lookAt(0, 0, 0);
-    return Promise.resolve([
-      environmentLight,
-      lightSourceTop,
-      lightSource2,
-      lightSource3,
-      lightSource4,
-    ]);
+    const lightSources = [environmentLight, lightSourceTop];
+    for (let i = 0; i < 3; i++) {
+      const lightSource = new RectAreaLight(0xffffff, 40, 100, 100);
+      const z = -500 * Math.cos((i * Math.PI * 2) / 3);
+      const x = 500 * Math.sin((i * Math.PI * 2) / 3);
+      console.log(x, z);
+      lightSource.position.set(x, 400, z);
+      lightSource.lookAt(0, 0, 0);
+      lightSources.push(lightSource);
+    }
+    return Promise.resolve(lightSources);
   }
 }
